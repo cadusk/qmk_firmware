f3a05b83fcfe48765fa8503e9464f2d765602e27
Carlos Cardoso <cadu@oscardoso.com.br>
2021-08-27 23:22:16 -0400

    Swap `grave` and `equal` to look like regular layouts

--- keyboards/kinesis/keymaps/cadusk/keymap.c
+++ keyboards/kinesis/keymaps/cadusk/keymap.c
@@ -72,7 +72,7 @@ QWERTY
 |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |                                     |  F9  |  F10 |  F11 |  F12 |PrtScr|ScrLck| Pause|Numpad|Adjust|
 `--------------------------------------------------------------'                                     `--------------------------------------------------------------'
 ,------------------------------------------------------.                                                     ,------------------------------------------------------.
-|    =    |    1   |    2   |    3   |    4   |    5   |                                                     |    6   |    7   |    8   |    9   |    0   |    -    |
+|    `    |    1   |    2   |    3   |    4   |    5   |                                                     |    6   |    7   |    8   |    9   |    0   |    -    |
 |---------+--------+--------+--------+--------+--------|                                                     |--------+--------+--------+--------+--------+---------|
 |   Tab   |    Q   |    W   |    E   |    R   |    T   |                                                     |    Y   |    U   |    I   |    O   |    P   |    \    |
 |---------+--------+--------+--------+--------+--------|                                                     |--------+--------+--------+--------+--------+---------|
@@ -80,7 +80,7 @@ QWERTY
 |---------+--------+--------+--------+--------+--------|                                                     |--------+--------+--------+--------+--------+---------|
 |  Shift  |    Z   |    X   |    C   |    V   |    B   |                                                     |    N   |    M   |    ,   |    .   |    /   |  Shift  |
 `---------+--------+--------+--------+--------+--------'                                                     `--------+--------+--------+--------+--------+---------'
-          |    `   |   Ins  |  Left  |  Right |                                                                       |   Up   |  Down  |    [   |    ]   |
+          |    =   |   Ins  |  Left  |  Right |                                                                       |   Up   |  Down  |    [   |    ]   |
           `-----------------------------------'                                                                       `-----------------------------------'
                                                      ,-----------------.                 ,-----------------.
                                                      | ESC/Ctl|  LAlt  |                 |  RAlt  |  RCtl  |
@@ -92,11 +92,11 @@ QWERTY
 */
 [_QWERTY] = LAYOUT_pretty(
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, NUMPAD,  ADJUST,
-  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   CTLESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-           KC_GRV,  KC_INS,  KC_LEFT, KC_RGHT,                                                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
+           KC_EQL,  KC_INS,  KC_LEFT, KC_RGHT,                                                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
                                                         CTLESC,  KC_LALT,                   KC_RALT, KC_RCTL,
                                                                  KC_DEL,                    KC_APP,
                                                KC_BSPC, KC_LGUI, KC_HOME,                   KC_END,  ENTNS,   SPCFN
@@ -109,7 +109,7 @@ Colemak
 |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |                                     |  F9  |  F10 |  F11 |  F12 |PrtScr|ScrLck| Pause|Numpad|Adjust|
 `--------------------------------------------------------------'                                     `--------------------------------------------------------------'
 ,------------------------------------------------------.                                                     ,------------------------------------------------------.
-|    =    |    1   |    2   |    3   |    4   |    5   |                                                     |    6   |    7   |    8   |    9   |    0   |    -    |
+|    `    |    1   |    2   |    3   |    4   |    5   |                                                     |    6   |    7   |    8   |    9   |    0   |    -    |
 |---------+--------+--------+--------+--------+--------|                                                     |--------+--------+--------+--------+--------+---------|
 |   Tab   |    Q   |    W   |    F   |    P   |    G   |                                                     |    J   |    L   |    U   |    Y   |    ;   |    \    |
 |---------+--------+--------+--------+--------+--------|                                                     |--------+--------+--------+--------+--------+---------|
@@ -117,7 +117,7 @@ Colemak
 |---------+--------+--------+--------+--------+--------|                                                     |--------+--------+--------+--------+--------+---------|
 |  Shift  |    Z   |    X   |    C   |    V   |    B   |                                                     |    K   |    M   |    ,   |    .   |    /   |  Shift  |
 `---------+--------+--------+--------+--------+--------'                                                     `--------+--------+--------+--------+--------+---------'
-          |    `   |   Ins  |  Left  |  Right |                                                                       |   Up   |  Down  |    [   |    ]   |
+          |    =   |   Ins  |  Left  |  Right |                                                                       |   Up   |  Down  |    [   |    ]   |
           `-----------------------------------'                                                                       `-----------------------------------'
                                                      ,-----------------.                 ,-----------------.
                                                      | ESC/Ctl|  LAlt  |                 |  RAlt  |  RCtl  |
@@ -129,11 +129,11 @@ Colemak
 */
 [_COLEMAK] = LAYOUT_pretty(
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, NUMPAD,  ADJUST,
-  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                                                          KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
   CTLESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                                                          KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                          KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-           KC_GRV,  KC_INS,  KC_LEFT, KC_RGHT,                                                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
+           KC_EQL,  KC_INS,  KC_LEFT, KC_RGHT,                                                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
                                                         CTLESC,  KC_LALT,                   KC_RALT, KC_RCTL,
                                                                  KC_DEL,                    KC_APP,
                                                KC_BSPC, KC_LGUI, KC_HOME,                   KC_END,  ENTNS,   SPCFN
