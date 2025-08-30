fd04f4ca28ddf679467e23181029134f095ea47b
Carlos Cardoso <cadu@oscardoso.com.br>
2025-08-30 19:57:40 -0400

    Add cardoc custom keymap for kinesis keyboard

---
+++ keyboards/kinesis/keymaps/cardoc/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Configures global tapping term
+#define TAPPING_TERM 180
+// Enables rapid switch from tap to hold, disables double tap hold auto-repeat
+#define QUICK_TAP_TERM 180
+
+#define TAPPING_TOGGLE 2
+
+#define COMBO_TERM 50
+
+// This is the default behavior for the Kinesis Advantage 2
+// #define IGNORE_MOD_TAP_INTERRUPT
