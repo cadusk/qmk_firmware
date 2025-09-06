38438a52178c922dbb79ee9e07a00ddd076f184e
Carlos Cardoso <cadu@oscardoso.com.br>
2025-09-05 20:30:38 -0400

    Remove Ctrl ModTap from KC_QUOTE

--- keyboards/kinesis/keymaps/cardoc/keymap.c
+++ keyboards/kinesis/keymaps/cardoc/keymap.c
@@ -12,7 +12,6 @@ enum custom_keycodes {
     ___X___  = KC_NO,                      // makes it easier to read than XXXXXXX
 
     CTL_ESC  = LCTL_T(KC_ESC),
-    CTL_QUOT = RCTL_T(KC_QUOT),
 
     FN_LANG  = LT(_FUNCTION, KC_SPACE),    // Change Mac language
     FN_TMUX  = LT(_FUNCTION, KC_B),        // Tmux leader key
@@ -29,7 +28,7 @@ enum custom_keycodes {
 * |---------+------+------+------+------+------|                           +------+------+------+------+------+--------|
 * | Tab     |   Q  |   W  |   E  |   R  |   T  |                           |   Y  |   U  |   I  |   O  |   P  | \|     |
 * |---------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* | Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | Ctrl/'"|
+* | Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"     |
 * |---------+------+------+------+------+------|                           |------+------+------+------+------+--------|
 * | Shift   |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift  |
 * `---------+------+------+------+------+-------                           `------+------+------+------+------+--------'
@@ -51,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, QK_BOOT,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_QUOT,
+    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
              KC_EQL,  KC_INS,  KC_LEFT, KC_RGHT,                                                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
                                                           KC_LCTL, KC_LALT,                   KC_RALT, KC_DEL,
