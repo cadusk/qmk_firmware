209f903d4d5a2b4fa9e4437b69a5014611a13375
Carlos Cardoso <cadu@oscardoso.com.br>
2025-11-29 01:02:18 -0500

    Keymap customizations

--- keyboards/kinesis/keymaps/cardoc/keymap.c
+++ keyboards/kinesis/keymaps/cardoc/keymap.c
@@ -9,53 +9,47 @@ enum kinesis_layers {
 };
 
 enum custom_keycodes {
-    ___X___  = KC_NO,                      // makes it easier to read than XXXXXXX
-
-    CTL_ESC  = LCTL_T(KC_ESC),
-
-    FN_LANG  = LT(_FUNCTION, KC_SPACE),    // Change Mac language
-    FN_TMUX  = LT(_FUNCTION, KC_B),        // Tmux leader key
+    ___X___  = KC_NO,                       // Makes it easier to read than XXXXXXX
+    FN_LANG  = LT(_FUNCTION, KC_SPACE),     // Change Mac language
+    MO_FUNC  = MO(_FUNCTION),               // Momentarily switches to _FUNCTION layer while key is held
 };
 
 /****************************************************************************************************
 *
 * Keymap: Default Layer in Qwerty
 *
-* ,--------------------------------------------------------------------------------------------------------------------.
-* | Esc     |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | PSCR | SLCK | PAUS |  FN0 |  BOOT  |
-* |---------+------+------+------+------+------+---------------------------+------+------+------+------+------+--------|
-* | `~      |  1!  |  2@  |  3#  |  4$  |  5%  |                           |  6^  |  7&  |  8*  |  9(  |  0)  | -_     |
-* |---------+------+------+------+------+------|                           +------+------+------+------+------+--------|
-* | Tab     |   Q  |   W  |   E  |   R  |   T  |                           |   Y  |   U  |   I  |   O  |   P  | \|     |
-* |---------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* | Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"     |
-* |---------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* | Shift   |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift  |
-* `---------+------+------+------+------+-------                           `------+------+------+------+------+--------'
-*           |  =+  | Ins  | Left | Right|                                         |  Up  | Down |  [{  |  ]}  |
-*           `---------------------------'                                         `---------------------------'
+* ,-------------------------------------------------------------------------------------------------------------------.
+* |        |      |      |      |      |      |      |      |      |      |      |      |      |      |      |  BOOT  |
+* |--------+------+------+------+------+------+---------------------------+------+------+------+------+------+--------|
+* | `~     |  1!  |  2@  |  3#  |  4$  |  5%  |                           |  6^  |  7&  |  8*  |  9(  |  0)  | -_     |
+* |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|
+* | Tab    |   Q  |   W  |   E  |   R  |   T  |                           |   Y  |   U  |   I  |   O  |   P  | \|     |
+* |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
+* | Esc    |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | '"     |
+* |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
+* | Shift  |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift  |
+* `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'
+*          |  =+  | Ins  | Left | Right|                                         |  Up  | Down |  [{  |  ]}  |
+*          `---------------------------'                                         `---------------------------'
 *                                        ,-------------.           ,-------------.
-*                                        | Ctrl | Alt  |           | Alt  | Del  |
+*                                        | Lang | Alt  |           | Alt  | Del  |
 *                                 ,------|------|------|           |------+------+------.
-*                                 |      |      | Hypr |           | Meh  |      |      |
-*                                 | BkSp | L_Gui|------|           |------|Return| Space|
-*                                 |      |      | Lang |           | TMUX |      |      |
+*                                 |      |      | FUNC |           | FUNC |      |      |
+*                                 | BkSp | GUI  |------|           |------|Return| Space|
+*                                 |      |      | Ctrl |           | Ctrl |      |      |
 *                                 `--------------------'           `--------------------'
-*                                                  ^------  Fn  ------^
-*   HYPER: Ctrl + Shift + Alt + Gui
-*   MEH:   Ctrl + Shift + Alt
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, QK_BOOT,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
              KC_EQL,  KC_INS,  KC_LEFT, KC_RGHT,                                                                         KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
-                                                          KC_LCTL, KC_LALT,                   KC_RALT, KC_DEL,
-                                                                   KC_HYPR,                   KC_MEH,
-                                                 KC_BSPC, KC_RGUI, FN_LANG,                   FN_TMUX, KC_ENTER, KC_SPC
+                                                          FN_LANG, KC_LALT,                   KC_RALT, KC_DEL,
+                                                                   MO_FUNC,                   MO_FUNC,
+                                                 KC_BSPC, KC_LGUI, KC_LCTL,                   KC_RCTL, KC_ENTER, KC_SPC
   ),
 
 /****************************************************************************************************
@@ -128,15 +122,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return true;
         }
-
-        case FN_TMUX: {
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(LCTL(KC_B));
-                return false;
-            }
-            return true;
-        }
-
     }
 
     return true;
